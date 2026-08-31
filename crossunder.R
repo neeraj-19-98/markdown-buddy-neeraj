c9be9feff9a098ddc22b7864a68d5bb57a94c93b
neeraj-19-98 <neerajbangarh143@gmail.com>
2026-08-31 01:52:51 -0400

    Add crossunder implementation

---
+++ crossunder.R
@@ -0,0 +1,19 @@
+crossunder <- function(x, y) {
+  result <- rep(FALSE, length(x))
+  
+  if (length(x) != length(y)) {
+    return(result)
+  }
+  
+  for (i in 2:length(x)) {
+    if (!is.na(x[i]) && !is.na(y[i]) &&
+        !is.na(x[i - 1]) && !is.na(y[i - 1])) {
+      
+      if (x[i] < y[i] && x[i - 1] >= y[i - 1]) {
+        result[i] <- TRUE
+      }
+    }
+  }
+  
+  return(result)
+}
